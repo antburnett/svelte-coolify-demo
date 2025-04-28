48b6df8b50722e356038c8afa1cf19520eb5484a
Ant Burnett <ant@burnett.com.au>
2025-04-28 13:34:34 +1000

    Test webhook

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	
-	let greeting = 'Hello, Coolify! This is an update!';
+	let greeting = 'Hello, Coolify! This is an update! Webhook it, baby';
 	let isDeployed = false;
 	
 	onMount(() => {
